ac0a98d2e904722ba9ac8f41a74af8da124911ae
Hekon-Doru <vytenis.kazlas@gmail.com>
2025-05-29 12:25:06 +0300

    pasidariau kad pagal tai koks kintamasis rasytu atitinkama spalva

--- nd/01/app.js
+++ nd/01/app.js
@@ -53,6 +53,15 @@ if (R1 == 0 || R2 == 0) {
 // gal užtektų sumažinti iki R1 mažiau už R2 bet nelygus nuliui, nelygybes zenkas !== (Not lygus nuliui - is not ?)
 
 // section 4
+console.log('-----------------');
+console.log('4 uzduotis');
+console.log('-----------------');
+// Sukurkite tris kintamuosius ir 
+// naudodamiesi funkcija rand() jiems 
+// priskirkite atsitiktines reikšmes nuo 50 iki 200. 
+// Iš section tage su id=go4 esančių div tagų 
+// padarykite atitinkamo dydžio (px) apskritimus. 
+// Apskritimai turi būti išdėlioti eilute nuo mažiausio iki didžiausio.
 
 const S41 = rand(50, 200);
 const S42 = rand(50, 200);
@@ -97,14 +106,78 @@ Circle2.style.backgroundColor = 'green';
 Circle3.style.width = sorted[2] + 'px';
 Circle3.style.height = sorted[2] + 'px';
 Circle3.style.borderRadius = '50%';
-Circle3.style.backgroundColor = 'green'; 
+Circle3.style.backgroundColor = 'green';
 
 const lygiavimas = document.querySelector('#go4');
 
 lygiavimas.style.display = 'flex';
 lygiavimas.style.justifyContent = 'space-around';
-lygiavimas.style.alignItems = 'center'; 
+lygiavimas.style.alignItems = 'center';
 lygiavimas.style.width = '100%';
+
+// 5 uzduotis 
+console.log('-----------------');
+console.log('5 uzduotis');
+console.log('-----------------');
+// Naudokite funkcija rand(). 
+// Į section tage su id=go5 esančius span įrašykite 3 skaičius nuo -10 iki 10. 
+// Skaičius mažesnių už 0 spausdinkite raudonai,  
+// didesnius už 0 mėlynai, 
+// o 0 žaliai.
+
+const S5_1 = rand(-10, 10);
+
+// pasičekinam skaiciu
+console.log(S5_1);
+
+/* turim tris spanus, 
+spana pasitargetinam su documen.querySelector('#go5 span'), kiti bus su + span,
+jaučiu reikia vėl į masyva detis viska ir iskart lygiuot
+abort idea, reikia tiesiog spalvint.
+
+klausimas jei visus spanus targetinu ir spausdinu juose reiksmes ar jos bus skirtingos */
+console.log(S5_1);
+console.log(S5_1);
+/* nope, visi vienodi, vadinas reikia pasidaryt array ? ar tiesiog targetint visus spanus nedarant kintamojo, bet su for each daryt? */
+console.log(rand(-10, 10));
+console.log(rand(-10, 10));
+
+/* ok 6itaip visi skirtingi.*/
+console
+  .log(rand(-10, 10));
+// for clarity galima ir taip rasytis
+
+// tai jei mazesnis skaicius bus raudonas tai bus 
+// if skaicius < 0 tai tada bus red
+// jei skaicius >  0  - blue
+// jei skaicius == 0 - green
+// reikia tada dar turbut ir true ir false naudot
+// 
+console.log('...');
+
+let kintamasis = rand(-10, 10);
+
+if (kintamasis < 0) {
+  console.log('red')
+} else if (kintamasis > 0) {
+  console.log('blue')
+} else if (kintamasis == 0) {
+  console.log('green')
+}
+console.log(kintamasis);
+
+//klausimas ar galim visa sita darytis kaip funkcija kuri pritaikoma kiekvienam
+
+
+
+/* 
+document.querySelectorAll('#go5 span') */
+//pasitargetinam visus spanus
+
+
+
+
+
 
 
 
