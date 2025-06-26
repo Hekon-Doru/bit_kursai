e25e0f35e0926c05d645411afb470e4c7ec6e3e4
Hekon-Doru <vytenis.kazlas@gmail.com>
2025-06-26 10:47:40 +0300

    cards

--- nd/04/app.js
+++ nd/04/app.js
@@ -1,38 +1,10 @@
-console.log('labas ibjektai');
-
-
-// ------------------------------ OBJEKTINIS PROGRAMAVIMAS ---------------------------- //
-
-// ------------ task 01
-// sukurti classe Du ir su juo sukurt objekta
-
-class Du {
-  constructor(turi) {
-    this.kiek = 2;
-    this.turi = turi;
-
-  }
-  daug() { //getter kuris kažką gražina
-    return this.kiek * this.turi;
-  }
-  keistiKiek(k) { //setter kuris paima
-    if (k <= 10) {
-      this.kiek = k;
-    }
-    return this;
-  }
-}
-
-const obj1 = new Du(53);
-const obj2 = new Du(33);
-
 /* console.log(obj1, obj2, obj1.daug(), obj2.daug(), obj2.keistiKiek(10));
 
 console.log(obj1, obj2, obj1.daug(), obj2.keistiKiek(10), obj2.daug());
 
 console.log(typeof Du, typeof obj1); */
 
-console.log(obj2.keistiKiek(70).kiek); //chaining
+
 
 
 
@@ -63,8 +35,29 @@ class Kibiras1 {
 }
 
 const kibiroObjektas1 = new Kibiras1();
+console.log('-----------');
 kibiroObjektas1.kiekPririnktaAkmenu();
-kibiroObjektas1.pridetiDaugAkmenu(99)
+console.log('-----------');
+kibiroObjektas1.pridetiDaugAkmenu(55)
 kibiroObjektas1.kiekPririnktaAkmenu();
+console.log('-----------');
+kibiroObjektas1.pridetiDaugAkmenu(55)
+kibiroObjektas1.kiekPririnktaAkmenu();
+console.log('-----------');
+kibiroObjektas1.prideti1Akmeni();
+kibiroObjektas1.kiekPririnktaAkmenu();
+console.log('-----------');
+kibiroObjektas1.prideti1Akmeni();
+kibiroObjektas1.kiekPririnktaAkmenu();
+console.log('-----------');
+kibiroObjektas1.prideti1Akmeni();
+kibiroObjektas1.kiekPririnktaAkmenu();
+console.log('-----------');
+kibiroObjektas1.prideti1Akmeni();
+kibiroObjektas1.kiekPririnktaAkmenu();
+console.log('-----------');
+kibiroObjektas1.prideti1Akmeni();
+kibiroObjektas1.kiekPririnktaAkmenu();
+console.log('-----------');
 kibiroObjektas1.prideti1Akmeni();
 kibiroObjektas1.kiekPririnktaAkmenu();
